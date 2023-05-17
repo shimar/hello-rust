bb7564345773bc7213de9fcaf99cdef95666d312
Ryohei Shima <shima01dev@gmail.com>
2023-05-17 21:04:50 +0900

    Add display example.

---
+++ examples/display.rs
@@ -0,0 +1,59 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "({}, {})", self.0, self.1)
+  }
+}
+
+#[derive(Debug)]
+struct Point2D {
+  x: f64,
+  y: f64,
+}
+
+impl fmt::Display for Point2D {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "x: {}, y: {}", self.x, self.y)
+  }
+}
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let vec = &self.0;
+
+    write!(f, "[")?;
+    for (count, v) in vec.iter().enumerate() {
+      if count != 0 {
+        write!(f, ", ")?;
+      }
+      write!(f, "{}:{}", count, v)?;
+    }
+    write!(f, "]")
+  }
+}
+
+fn main() {
+  let minmax = MinMax(0, 14);
+
+  println!("Compare structures:");
+  println!("Display: {}", minmax);
+  println!("Debug: {:?}", minmax);
+
+  let big_range = MinMax(-300, 300);
+  let small_range = MinMax(-3, 3);
+  println!("The big range is {big} and small range is {small}", big = big_range, small = small_range);
+
+  let point = Point2D{ x: 3.3, y: 7.2 };
+  println!("Compare points:");
+  println!("Display: {}", point);
+  println!("Debug: {:?}", point);
+
+  let v = List(vec![1, 2, 3]);
+  println!("{}", v);
+}
