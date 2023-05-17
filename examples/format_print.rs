7c47c7d5a4bf85b71f282e1eb4a630ad10628c20
Ryohei Shima <shima01dev@gmail.com>
2023-05-17 20:46:45 +0900

    Add format_print example.

---
+++ examples/format_print.rs
@@ -0,0 +1,14 @@
+fn main() {
+  println!("{} days!", 31);
+  println!("{0}, this is {1}. {1}, this is {0}.", "Alice", "Bob");
+
+  println!("{subject} {verb} {object}",
+           subject="the quick brown fox",
+           verb="jumps over",
+           object="the lazy dog");
+  println!("Base 10:         {}", 69420);
+  println!("Base  2(binary): {:b}", 69420);
+  println!("Base  8(octal):  {:o}", 69420);
+  println!("Base 16(hex):    {:x}", 69420);
+  println!("Base 16(HEX):    {:X}", 69420);
+}
