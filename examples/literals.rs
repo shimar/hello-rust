ee2ba5569796c2ead34eefe8876307e82e35ada1
Ryohei Shima <shima01dev@gmail.com>
2023-06-07 20:21:22 +0900

    Add literals example.

---
+++ examples/literals.rs
@@ -0,0 +1,16 @@
+use std::mem;
+
+fn main() {
+  let x = 1u8;
+  let y = 2u32;
+  let z = 3f32;
+
+  let i = 1;
+  let f = 1.0;
+
+  println!("size of `x` in bytes: {}", mem::size_of_val(&x));
+  println!("size of `y` in bytes: {}", mem::size_of_val(&y));
+  println!("size of `z` in bytes: {}", mem::size_of_val(&z));
+  println!("size of `i` in bytes: {}", mem::size_of_val(&i));
+  println!("size of `f` in bytes: {}", mem::size_of_val(&f));
+}
