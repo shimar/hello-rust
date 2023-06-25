ded150df8423fa2bb0f11e39d67bf2ec86ef145e
Ryohei Shima <shima01dev@gmail.com>
2023-06-25 15:19:36 +0900

    Add closures example.

---
+++ examples/closures.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let outer_var = 42;
+
+  let closure_annotated = |i: i32| -> i32 { i + outer_var };
+  let closure_inferred = |i| i + outer_var;
+
+  println!("closure_annotated: {}", closure_annotated(1));
+  println!("closure_inferred: {}", closure_inferred(1));
+
+  let one = || 1;
+  println!("closure returning one; {}", one());
+}
