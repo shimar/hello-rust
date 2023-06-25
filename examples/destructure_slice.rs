273e8ad23b4d7ec5d3083c2a277a815263e8e2a9
Ryohei Shima <shima01dev@gmail.com>
2023-06-25 14:28:28 +0900

    Add destructure_slice example.

---
+++ examples/destructure_slice.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let array = [3, -2, 6];
+
+  match array {
+    [0, second, third] =>
+      println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+    [1, _, third] => println!("array[0] = 1, array[2] = {}, and array[1] was ignored", third),
+    [-1, second, ..] => println!("array[0] = -1, array[1] = {}, and all the other ones were ignored", second),
+    [3, second, tail @ ..] => println!("array[0] = 3, array[1] = {} and the other elements were {:?}", second, tail),
+    [first, middle @ .., last] => println!("array[0] = {}, middle = {:?}, array[2] = {}", first, middle, last),
+  }
+}
